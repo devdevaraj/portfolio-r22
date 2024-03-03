9eed99b8a7372143c6b38f3c6b41733f65532364
Devaraj <devarajperayil327@gmail.com>
2024-03-03 23:09:26 +0530

    timeline

---
+++ src/components/3d/timeline-scene.tsx
@@ -0,0 +1,52 @@
+import { GroupProps, MeshProps } from "@react-three/fiber";
+import { Group, Vector3 } from "three";
+import { useFrame, useThree } from "@react-three/fiber";
+import { Ref, useRef } from "react";
+
+const _cubes = [
+ { pos: [-50, 0, 0], scale: 2 },
+ { pos: [50, 0, -50], scale: 4 },
+ { pos: [-50, 0, -100], scale: 2 },
+ { pos: [50, 0, -150], scale: 4 },
+ { pos: [-50, 0, -200], scale: 2 },
+ { pos: [50, 0, -250], scale: 4 },
+ { pos: [-50, 0, -300], scale: 2 },
+ { pos: [50, 0, -350], scale: 4 },
+ { pos: [-50, 0, -400], scale: 2 },
+ { pos: [50, 0, -450], scale: 4 },
+ { pos: [-50, 0, -500], scale: 2 },
+];
+
+export default function TimelineScene({innerRef}: {innerRef: Ref<Group>}) {
+  const ref = useRef<Group>(null);
+ const { pointer, camera } = useThree();
+ useFrame((state, delta) => {
+  const x = pointer.x / window.innerWidth;
+  const y = pointer.y / window.innerHeight;
+  console.log(x,y);
+  camera.rotation.set(x*100, y*100, 0);
+ });
+ return (
+  <group ref={innerRef}>
+   <ambientLight intensity={1} />
+   <pointLight position={[0, 0, 500]} intensity={6} decay={0} />
+   {_cubes.map((item, index) => (
+    <Cube
+     key={index}
+     scale={item.scale}
+     position={new Vector3(...item.pos)} />
+   ))}
+
+  </group>
+ );
+}
+
+
+function Cube(props:MeshProps) {
+ return (
+  <mesh {...props}>
+   <boxGeometry args={[10, 10, 10]} />
+   <meshStandardMaterial color={'orange'} />
+  </mesh>
+ );
+}
--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,10 +1,14 @@
 import { useRef } from "react";
 import { scrollTop, clientHeight } from "./scrollbar";
 import { effect } from "@preact/signals-react";
+import { Canvas, GroupProps } from "@react-three/fiber";
+import TimelineScene from "./3d/timeline-scene";
+import { Group } from "three";
 
 export default function Timeline() {
  const ref = useRef<HTMLElement>(null);
- const innerRef = useRef<HTMLElement>(null);
+ const innerRef = useRef<Group>(null);
+ let scroll = 0;
  effect(() => {
   const info = ref.current?.getBoundingClientRect();
   const height = info?.height;
@@ -13,17 +17,22 @@ export default function Timeline() {
   const OS = scrollTop.value - TS;
   const OH = height! - clientHeight.value;
   const TL = TS > scrollTop.value ? 0 : OS > OH ? OH : OS;
-  const scroll = TL / OH;
-  if (innerRef.current) innerRef.current.style.width = (scroll * 100 + "%");
+  scroll = TL / OH;
+  console.log(scroll);
+  if (innerRef.current) innerRef.current.position.set(0, 0, 500 * scroll);
  })
  return (
   <main
    ref={ref}
-   className="h-[200vh] relative">
+   className="h-[500vh] relative">
    <section
-    ref={innerRef}
-    className="sticky top-0 h-screen bg-slate-400 text-3xl transition duration-500">
-    timeline
+    className="sticky top-0 h-screen text-3xl transition duration-500">
+    <Canvas camera={{ position: [0, 0, 50] }}>
+     <TimelineScene
+      innerRef={innerRef}
+     // position={[0, 0, 500 * scroll]}
+     />
+    </Canvas>
    </section>
   </main>
  );
