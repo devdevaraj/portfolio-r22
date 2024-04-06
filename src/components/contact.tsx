646f06788b4c54fae2a42c87acf5419ca6b1208d
Devaraj <devarajperayil327@gmail.com>
2024-04-06 21:43:05 +0530

    contact

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,7 +1,30 @@
 export default function Contact() {
- return(
+ return (
   <menu className="w-full p-5 sm:p-9 text-white">
-   Contact me
+   <h2 className="font-bold text-2xl mb-4">CONTACT ME</h2>
+   <form onSubmit={e => e.preventDefault()} className="flex flex-col gap-4 opacity-90">
+    <input
+     type="text"
+     name="name"
+     placeholder="name"
+     className="h-10 rounded-md outline-none p-2 shadow-md shadow-white text-black"
+    />
+    <input
+     type="email"
+     name="email"
+     placeholder="email"
+     className="h-10 rounded-md outline-none p-2 shadow-md shadow-white text-black"
+    />
+    <textarea
+     name="message"
+     id="message"
+     className="resize-none h-32 rounded-md outline-none p-2 shadow-md shadow-white text-black"
+     placeholder="messgae"
+    ></textarea>
+    <button
+     className="h-10 bg-white text-black rounded-md shadow-md shadow-white"
+    >SEND</button>
+   </form>
   </menu>
  );
 }
