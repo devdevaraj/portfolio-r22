4f4a914a3a37882ca1984b643fb418cb421241f9
Devaraj <devarajperayil327@gmail.com>
2024-07-01 21:25:56 +0530

    My skill page

--- src/components/main-focus.jsx
+++ src/components/main-focus.jsx
@@ -1,6 +1,6 @@
 export default function MainFocus() {
  return(
-  <main className="w-svw h-screen">
+  <main className="w-svw">
    <section className="text-white">Main focus</section>
   </main>
  );
