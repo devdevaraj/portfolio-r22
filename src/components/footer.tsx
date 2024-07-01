8c06efc3b1c82bd29aa98dc49f8ffad6b861341c
Devaraj <devarajperayil327@gmail.com>
2024-07-01 22:38:44 +0530

    My skill page

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
  return(
-  <footer className="w-full h-96 p-5 sm:p-9 bg-gradient-to-t to-transparent from-black">
+  <footer className="w-full h-96 px-5 py-2 sm:px-9 sm:py-4 bg-gradient-to-t to-transparent flex items-end justify-center from-black">
    footer
+   <section className="text-white">Powered by Dvaraj P V  |  Copyright @ devaraj.tech</section>
   </footer>
  );
 }
